d1f4dd95d2d4ef992fde6f7029274b97fb43f0d4
Forgrul <hngieng9@gmail.com>
2023-03-30 23:15:44 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>
+        Hello, my name is 嚴詩順, my student ID is B10902100.
+        Thanks for reading.
+      </h1>
+    </div>
+  )
+}
+
+export default App
